7d21d1e0cd8d4ee6aee2a9bfebf01b6a44de5faa
PantherValley13 <Churchdarius@gmail.com>
2025-09-23 23:07:30 -0400

    Initial Commit

---
+++ currency converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency converter
+//
+//  Created by Darius on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
